4048674d14e0e909b1e5eb465ada7f134ce511ca
Ben G <bgilross@gmail.com>
2025-08-30 20:31:11 -0700

    added dropdown for lesson select, add basic display for sentence info and untranslated sentence

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -5,8 +5,49 @@ import { useDataStore } from "@/data/dataStore"
 
 const Main = () => {
 	const lessons = useDataStore((state) => state.lessons)
+	const currentLessonIndex = useDataStore((state) => state.currentLessonIndex)
+	const currentSentenceIndex = useDataStore(
+		(state) => state.currentSentenceIndex
+	)
+	const startNewLesson = useDataStore((state) => state.startNewLesson)
 
-	return <div>Main</div>
+	const handleLessonChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		const index = parseInt(e.target.value)
+		startNewLesson(index)
+	}
+
+	return (
+		<div>
+			<select
+				value={currentLessonIndex}
+				onChange={handleLessonChange}
+				className="px-2 py-1 border rounded"
+			>
+				{lessons.map((lesson, i) => (
+					<option
+						key={i}
+						value={i}
+					>
+						Lesson {lesson.lesson} - {lesson.details}
+					</option>
+				))}
+			</select>
+			<div>
+				<p>Current Lesson: {lessons[currentLessonIndex].name}</p>
+				<p>
+					Current Sentence Index: {currentSentenceIndex + 1} /{" "}
+					{lessons[currentLessonIndex].sentences?.length}
+				</p>
+				<p>
+					Sentence:{" "}
+					{
+						lessons[currentLessonIndex].sentences?.[currentSentenceIndex]
+							?.sentence
+					}
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default Main
--- src/data/dataStore.ts
+++ src/data/dataStore.ts
@@ -4,11 +4,21 @@ import type { Lesson } from "./types"
 
 interface DataStore {
 	lessons: Lesson[]
+	currentLessonIndex: number
+	currentSentenceIndex: number
 	// sentences: Sentence[]
+	startNewLesson: (index: number) => void
 }
 
 const lessons = spanishData.lessons
+const currentLessonIndex = 0
+const currentSentenceIndex = 0
 
 export const useDataStore = create<DataStore>((set, get) => ({
 	lessons,
+	currentLessonIndex,
+	currentSentenceIndex,
+	startNewLesson: (index: number) => {
+		set({ currentLessonIndex: index, currentSentenceIndex: 0 })
+	},
 }))
