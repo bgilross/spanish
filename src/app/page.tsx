340a4895d86a20803161488d710d3efe0cb6a76f
Ben G <bgilross@gmail.com>
2025-09-10 09:36:26 -0700

    updated title/metadata to read Spanish Leaner instead of create-next-app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@
 import MainPage from "@/components/MainPage"
 import FirstVisitModal from "@/components/FirstVisitModal" // added
 
+export const metadata = {
+	title: "Spanish Learner",
+}
+
 export default function Home() {
 	return (
 		<div>
